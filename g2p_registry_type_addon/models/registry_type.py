985b0ecb1eed3648317deb3e8627d1a3c63e25ba
tahzeer <tahzeer.ash@gmail.com>
2025-09-03 03:15:58 +0530

    style: reorder registry models in view

--- g2p_registry_type_addon/models/registry_type.py
+++ g2p_registry_type_addon/models/registry_type.py
@@ -2,16 +2,16 @@ from enum import Enum
 
 
 class G2PRegistryType(Enum):
-    RATION_CARD_APPLICANTS = "ration_card_applicants"
-    INDIVIDUALS = "individuals"
     FAMILIES = "families"
-    ELEC_MONTHLY_AVG = "elec_monthly_avg"
-    GOVT_EMPLOYEES = "govt_employees"
+    INDIVIDUALS = "individuals"
     LAND_HOLDINGS = "land_holdings"
-    LRS_BRS_APPLICATION = "lrs_brs_application"
-    PROFESSIONAL_TAX_PAYERS = "professional_tax_payers"
-    RESIDENTIAL_HOUSES = "residential_houses"
     VEHICLE_OWNERSHIP = "vehicle_ownership"
+    RESIDENTIAL_HOUSES = "residential_houses"
+    LRS_BRS_APPLICATION = "lrs_brs_application"
+    RATION_CARD_APPLICANTS = "ration_card_applicants"
+    PROFESSIONAL_TAX_PAYERS = "professional_tax_payers"
+    GOVT_EMPLOYEES = "govt_employees"
+    ELEC_MONTHLY_AVG = "elec_monthly_avg"
     OTHER = "other"
 
     @classmethod
@@ -25,16 +25,16 @@ class G2PTargetModelMapping:
     """Static mapping from registry type key to model name."""
 
     MODEL_MAPPING = {
-        "ration_card_applicants": "g2p.registry.ration.card.applicants",
-        "individuals": "g2p.registry.individuals",
         "families": "g2p.registry.families",
-        "elec_monthly_avg": "g2p.registry.elec.monthly.avg",
-        "govt_employees": "g2p.registry.govt.employees",
+        "individuals": "g2p.registry.individuals",
         "land_holdings": "g2p.registry.land.holdings",
-        "lrs_brs_application": "g2p.registry.lrs.brs.application",
-        "professional_tax_payers": "g2p.registry.professional.tax.payers",
-        "residential_houses": "g2p.registry.residential.houses",
         "vehicle_ownership": "g2p.registry.vehicle.ownership",
+        "residential_houses": "g2p.registry.residential.houses",
+        "lrs_brs_application": "g2p.registry.lrs.brs.application",
+        "ration_card_applicants": "g2p.registry.ration.card.applicants",
+        "professional_tax_payers": "g2p.registry.professional.tax.payers",
+        "govt_employees": "g2p.registry.govt.employees",
+        "elec_monthly_avg": "g2p.registry.elec.monthly.avg",
     }
 
     @classmethod
