a5746caeaa485cd0b4ab333c74c90d106daa54ad
tahzeer <tahzeer.ash@gmail.com>
2025-09-03 15:01:36 +0530

    fix: update model mappings

--- g2p_registry_type_addon/models/registry_type.py
+++ g2p_registry_type_addon/models/registry_type.py
@@ -6,8 +6,6 @@ class G2PTargetModelMapping:
     """Static mapping from registry type key to model name."""
 
     MODEL_MAPPING = {
-        "student": "g2p.student.registry",
-        "farmer": "g2p.farmer.registry",
         "worker": "g2p.worker.registry",
         "worker_daily": "g2p.worker.daily.registry",
         "worker_monthly": "g2p.worker.monthly.registry",
@@ -20,8 +18,6 @@ class G2PTargetModelMapping:
 
 
 class G2PRegistryType(Enum):
-    FARMER = "farmer"
-    STUDENT = "student"
     WORKER = "worker"
     WORKER_DAILY = "worker_daily"
     WORKER_MONTHLY = "worker_monthly"
